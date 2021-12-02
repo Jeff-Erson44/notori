0f3e540d5b2c7a9dbc133690ba8f24c31da9260c
Elisabeth Ngo <elisabeth.ngo@jasparo.fr>
2021-12-02 10:14:19 +0100

    fonts

--- public/includes/header.php
+++ public/includes/header.php
@@ -10,6 +10,25 @@
             <span class="sr-only"></span>
         </button>
 
-        
+
+        <div class="header__three--links">
+            <ul id="primary-navigation" data-visible="false" class="primary-navigation">
+                <li class="active">
+                    <a href="#description">À &nbsp;propos</a>
+                </li>
+                <li class="active">
+                    <a href="">Étapes</a>
+                </li>
+                <li class="active">
+                    <a href="#faq">FAQ</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="header__link--contact">
+            <li class="active">
+                <a href="#contact">Contact</a>
+            </li>
+        </div>
     </nav>
 </header>
