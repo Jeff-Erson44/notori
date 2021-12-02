26d7a2a68777950850ac9d1207f95a5ae8b2c2df
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-02 11:20:13 +0100

    footer + responsive

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php require_once('public/includes/head.php')?>
 <?php require_once('public/includes/header.php')?>
 
+
+<?php require_once('public/includes/footer.php')?>
+
 <script src="assets/js/script.js"></script>
---
+++ public/includes/footer.php
@@ -0,0 +1,37 @@
+<div class="footer__contain">
+    <div class="footer__container--logo">
+        <figure>
+            <img class="logo__footer" src="assets/img/logo_black.svg" alt="logo imminence noir et blanc">
+        </figure>
+    </div>
+    <div class="footer__container--navigation">
+        <h5 class="footer__container--title">navigation</h5>
+        <div class="footer__navigation--link">
+            <p class="navigation__link"><a href="">Accueil</a></p>
+            <p class="navigation__link"><a href="">À propos</a></p>
+            <p class="navigation__link"><a href="">Étapes</a></a></p>
+            <p class="navigation__link"><a href="">FAQ</a></p>
+        </div>
+        
+    </div>
+    <div class="footer__container--social">
+        <h5 class="footer__container--title">social</h5>
+        <div class="footer__social--link">
+            <p class="social__link"><a href="">LinkedIn</p></a>
+            <p class="social__link"><a href="">Facebook</p></a>
+            <p class="social__link"><a href="">Instagram</p></a>
+        </div>
+
+    </div>
+    <div class="footer__container--adress">
+        <div class="container_adress--location">
+            <h5 class="footer__container--title">Location</h5>
+                <p class="adress__text">8 bis Rue de la Fontaine au Roi</p>
+                <p class="adress__text">75011 Paris</p>
+        </div>
+        <div class="container_adress--mail">
+            <h5 class="footer__container--title">Contact</h5>
+                <p class="mail__text">7xxxxx@xxxgmail.com</p>
+        </div>
+    </div>
+</div>
