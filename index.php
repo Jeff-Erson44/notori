bc723a244a1092be8d55de3518144f054aa100e2
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-02 16:50:50 +0100

    counter + home + responsive

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php require_once('public/includes/head.php')?>
+
 <?php require_once('public/includes/header.php')?>
+
 <?php require_once('public/includes/home.php')?>
 
 <?php require_once('public/includes/apropos.php')?>
+
 <?php require_once('public/includes/counter.php')?>
 <?php require_once('public/includes/faq.php')?>
-
 <?php require_once('public/includes/footer.php')?>
 
 <script src="assets/js/script.js"></script>
