e3b5f5d07c6e9de424a4e4a4014c190f3dac1b1f
AntoineSalleras <antoinesalleras@hotmail.com>
2021-12-02 10:48:18 +0100

    new faq

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php require_once('public/includes/head.php')?>
 <?php require_once('public/includes/header.php')?>
+<?php require_once('public/includes/faq.php')?>
 
 <script src="assets/js/script.js"></script>
