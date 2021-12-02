bd2003b0902574400b395d8b7f2e639e81685653
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-02 15:05:35 +0100

    apropos + responsive

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require_once('public/includes/head.php')?>
 <?php require_once('public/includes/header.php')?>
+
+<?php require_once('public/includes/apropos.php')?>
 <?php require_once('public/includes/faq.php')?>
 
 <?php require_once('public/includes/footer.php')?>
