dfd1c6fb61a3dfa9fa471954192e20fdb9e50eb3
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-03 11:45:29 +0100

    pourquoi

--- public/includes/pourquoi.php
+++ public/includes/pourquoi.php
@@ -1,19 +1,40 @@
 <section id="pourquoi">
-    <h2 class="subtitle">Pourquoi imminence</h2>
-    <div class="pourquoi__container">
-        <div class="pourquoi__container--left">
-            <p class="pourquoi__container--text">
+    <h2 class="subtitle">Pouquoi Imminence ?</h2>
+    <div class="pourquoi__contain">
+        <div class="pourquoi__contain--image">
+            <figure>
+                <img src="assets/img/illu.png" alt="#######">
+            </figure>
+            <figure>
+                <img src="assets/img/illu.png" alt="#######">
+            </figure>
+        </div>
+        <div class="pourquoi__contain--text">
+            <p class="contain__text--highlight">
             Nous avons constaté qu’il peut être fastudieux pour vous de faire vos démarches administratives pour devenir entrepreuneur. Afin de vous faciliter votre processus de recherche, “imminence” a vu le jour.
             </p>
-
-            <p class="pourquoi__container--text">
+            <p class="contain__text--highlight">
             Avec ce dispositif, vous allez enfin pouvoir gagner du temps et de l’énergie, et vous pourrez mieux vous consacrer à votre projet. 
             </p>
         </div>
-        <div class="pourquoi__container--right">
+    </div>
+
+    <div class="pourquoi__contain">
+        <div class="pourquoi__contain--image">
             <figure>
-                <img src="assets/img/img1.jpg" alt="" classe="pourquoi__container--img">
+                <img src="assets/img/illu.png" alt="#######">
+            </figure>
+            <figure>
+                <img src="assets/img/illu.png" alt="#######">
             </figure>
         </div>
+        <div class="pourquoi__contain--text">
+            <p class="contain__text--highlight">
+            Nous avons constaté qu’il peut être fastudieux pour vous de faire vos démarches administratives pour devenir entrepreuneur. Afin de vous faciliter votre processus de recherche, “imminence” a vu le jour.
+            </p>
+            <p class="contain__text--highlight">
+            Avec ce dispositif, vous allez enfin pouvoir gagner du temps et de l’énergie, et vous pourrez mieux vous consacrer à votre projet. 
+            </p>
+        </div>
     </div>
-</section>
+</section> 
