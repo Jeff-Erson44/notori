7ff183790191bcd2be8c62e8ed72e392a6c51c39
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-03 14:41:41 +0100

    apropos

--- public/includes/apropos.php
+++ public/includes/apropos.php
@@ -7,7 +7,6 @@
         <div class="apropos__container--text">
             <p>“Imminence” est un dispositif créé spécialement pour aider les futurs entrepreneurs innovants, mais aussi les entrepreneurs en général.</p>
             <p>Si vous souhaitez vous lancer, mais que vous ne savez pas quelles sont les informations qui vous concernent, alors vous venez de trouver ce qu’il vous faut ! </p>
-
         </div>
     </div>
 </section>
