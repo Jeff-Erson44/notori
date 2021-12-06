7f7d5e4e7b634b5d52d6e1cacd190b0e397b1ceb
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-07 00:11:24 +0100

    faq

--- public/includes/faq.php
+++ public/includes/faq.php
@@ -1,55 +1,53 @@
 <section id="faq" class="faq__section">
-
-    <div class="faq__div--first">
-        <h2>F.A.Q: Comment "Imminence" vous aide ?</h2>
-
-        <div class="faq">
-            <div class="question">
-                <h3>Comment retrouver les informations qui me concerne ?</h3>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"/></svg>
-            </div>
-
-            <div class="answer">
-                <p>
-                Avec notre système de filtre, où vous pouvez indiquer votre âge, votre genre, votre statut, votre secteur d’activité ainsi que votre localisation, vous accédez facilement aux informations qui vous correspondent.
-                </p>
-            </div>
-        </div>
-        <div class="faq">
-            <div class="question">
-                <h3>Comment développer mon propre réseau ?</h3>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"/></svg>
-            </div>
-
-            <div class="answer">
-                <p>
-                Grâce à notre filtre personnalisé, retrouvez tous les groupes et pages existants sur les divers réseaux sociaux. Un forum est également mis en place pour que vous puissiez poser toutes vos questions, et notre communauté, qui s’agrandit chaque jour, vous partagera leurs expériences. 🤝
-                </p>
+    <h2>F.A.Q: Comment "Imminence" vous aide ?</h2>
+    <div class="faq__container">
+        <div class="faq__container--card">
+            <div class="faq__card">
+                <div class="faq__card--front">
+                    <div class="faq__container--img pic-1"></div>
+                    <h4 class="faq__container--text">Comment retrouver les informations qui me concernent ?</h4>
+                    <img src="assets/img/flip-icon.svg" alt="icone flip" class="faq-flip">
+                </div>
+                <div class="faq__card--back">
+                    <h5>Comment retrouver les informations qui me concernent ?</h5>
+                    <p class="faq__container--text">Avec notre système de filtre, où vous pouvez indiquer votre âge, votre genre, votre statut, votre secteur d’activité ainsi que votre localisation, vous accédez facilement aux informations qui vous correspondent.</p>
+                    <a href="#" class="faq__button">Je filtre mes infos</a>
+                </div>
             </div>
         </div>
-        <div class="faq">
-            <div class="question">
-                <h3>Comment puis-je rencontrer des personnes qui pourraient m’aider ? </h3>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"/></svg>
-            </div>
-
-            <div class="answer">
-                <p>
-                Des webinaires et des salons sont organisés pour que vous puissiez échanger et faire de nouvelles rencontres. Découvrez les prochaines dates.
-                </p>
-            </div>
-        </div>
-        <div class="faq">
-            <div class="question">
-                <h3>Comment gérer la partie administrative ?</h3>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"/></svg>
-            </div>
-
-            <div class="answer">
-                <p>
-                Avec votre compte, accédez à tous vos documents stockés dans notre base de données. De plus, un agenda personnalisé vous permet de suivre vos démarches administratives pour ne rien manquer. 📅
-                </p>
+        <div class="faq__container--card">
+            <div class="faq__card">
+                <div class="faq__card--front">
+                    <div class="faq__container--img pic-2"></div>
+                    <h4 class="faq__container--text">Comment développer mon propre réseau ?</h4>
+                    <img src="assets/img/flip-icon.svg" alt="icone flip" class="faq-flip">
+                </div>
+                <div class="faq__card--back">
+                    <h5>Comment développer mon propre réseau ?</h5>
+                    <p class="faq__container--text">Grâce à notre filtre personnalisé, retrouvez tous les groupes et pages existants sur les divers réseaux sociaux. Un forum est également mis en place pour que vous puissiez poser toutes vos questions, et notre communauté vous partagera leurs expériences.</p>
+                    <a href="#" class="faq__button">Je filtre mes infos</a>
+                </div>
             </div>
         </div>
+        <div class="faq__container--card">
+            <div class="faq__card">
+                <div class="faq__card--front">
+                    <div class="faq__container--img pic-3"></div>
+                    <h4 class="faq__container--text">Comment puis-je rencontrer des personnes qui pourraient m'aider ?</h4>
+                    <img src="assets/img/flip-icon.svg" alt="icone flip" class="faq-flip">
+                </div>
+                <div class="faq__card--back">
+                    <h5>Comment puis-je rencontrer des personnes qui pourraient m'aider ?</h5>
+                    <p class="faq__container--text">Des webinaires et des salons sont organisés pour que vous puissiez échanger et faire de nouvelles rencontres.
+
+                    Des formations sont proposées pour vous apprendre à remplir les divers documents.</p>
+                    <a href="#" class="faq__button">Je filtre mes infos</a>
+                </div>
+            </div>
+        </div>   
     </div>
 </section>
+
+
+
+
--- public/includes/footer.php
+++ public/includes/footer.php
@@ -3,17 +3,21 @@
         <figure>
             <img class="logo__footer" src="assets/img/logo_black.svg" alt="logo imminence noir et blanc">
         </figure>
+        <p class="container__logo--text header5 ">
+        Informations, aides, démarches à suivre, actualités, communauté, forum, événements, formations...<br>Tout ce qui vous concernent et qui vous correspondent, regroupé au même endroit !
+        </p>
     </div>
     <div class="footer__container--navigation">
         <h5 class="footer__container--title">navigation</h5>
         <div class="footer__navigation--link">
             <p class="navigation__link"><a href="">Accueil</a></p>
             <p class="navigation__link"><a href="">À propos</a></p>
-            <p class="navigation__link"><a href="">Étapes</a></a></p>
+            <p class="navigation__link"><a href="">Pour vous</a></a></p>
             <p class="navigation__link"><a href="">FAQ</a></p>
         </div>
         
     </div>
+
     <div class="footer__container--social">
         <h5 class="footer__container--title">social</h5>
         <div class="footer__social--link">
@@ -21,11 +25,11 @@
             <p class="social__link"><a href="">Facebook</p></a>
             <p class="social__link"><a href="">Instagram</p></a>
         </div>
-
     </div>
+
     <div class="footer__container--adress">
         <h5 class="footer__container--title">Contact</h5>
-            <p class="mail__text" mailto="">7xxxxx@xxxgmail.com</p>
+            <a class="mail__text" href="mailto=contact@imminence.com">contact@imminence.com</a>
     </div>
 </div>
 <div class="copyright__contain">
