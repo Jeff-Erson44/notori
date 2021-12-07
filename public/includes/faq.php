77472b7385027eeda27aeec5fd9ae3becfc0b15c
Elisabeth Ngo <elisab.ngo@gmail.com>
2021-12-08 00:13:04 +0100

    faq responsive

--- public/includes/faq.php
+++ public/includes/faq.php
@@ -1,5 +1,5 @@
-<section id="faq" class="faq__section">
-    <h2>F.A.Q: Comment "Imminence" vous aide ?</h2>
+<h2 class="faq__title">F.A.Q: Comment imminence vous aide ?</h2>
+<section id="faq">
     <div class="faq__container">
         <div class="faq__container--card">
             <div class="faq__card">
@@ -45,6 +45,68 @@
                 </div>
             </div>
         </div>   
+    </div>
+</section>
+<!-- Version mobile -->
+<section id="faq__mobile">
+            <div class="faq__container">
+                 <!-- Swiper -->
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <!-- card slide-->
+                <div class="swiper-slide">
+                    <div class="faq__container--card">
+                        <div class="faq__card">
+                            <div class="faq__card--front">
+                                <div class="faq__container--img pic-1"></div>
+                                <h4 class="faq__container--text">Comment retrouver les informations qui me concernent ?</h4>
+                                <img src="assets/img/icon_flip.svg" alt="icone flip" class="faq-flip">
+                            </div>
+                            <div class="faq__card--back">
+                                <h5>Comment retrouver les informations qui me concernent ?</h5>
+                                <p class="faq__container--text">Avec notre système de filtre, où vous pouvez indiquer votre âge, votre genre, votre statut, votre secteur d’activité ainsi que votre localisation, vous accédez facilement aux informations qui vous correspondent.</p>
+                                <a href="#" class="faq__button">Je filtre mes infos</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="faq__container--card">
+                        <div class="faq__card">
+                            <div class="faq__card--front">
+                                <div class="faq__container--img pic-2"></div>
+                                <h4 class="faq__container--text">Comment développer mon propre réseau ?</h4>
+                                <img src="assets/img/icon_flip.svg" alt="icone flip" class="faq-flip">
+                            </div>
+                            <div class="faq__card--back">
+                                <h5>Comment développer mon propre réseau ?</h5>
+                                <p class="faq__container--text">Grâce à notre filtre personnalisé, retrouvez tous les groupes et pages existants sur les divers réseaux sociaux. Un forum est également mis en place pour que vous puissiez poser toutes vos questions, et notre communauté vous partagera leurs expériences.</p>
+                                <a href="#" class="faq__button">Je filtre mes infos</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="faq__container--card">
+                        <div class="faq__card">
+                            <div class="faq__card--front">
+                                <div class="faq__container--img pic-3"></div>
+                                <h4 class="faq__container--text">Comment puis-je rencontrer des personnes qui pourraient m'aider ?</h4>
+                                <img src="assets/img/icon_flip.svg" alt="icone flip" class="faq-flip">
+                            </div>
+                            <div class="faq__card--back">
+                                <h5>Comment puis-je rencontrer des personnes qui pourraient m'aider ?</h5>
+                                <p class="faq__container--text">Des webinaires et des salons sont organisés pour que vous puissiez échanger et faire de nouvelles rencontres.
+
+                                Des formations sont proposées pour vous apprendre à remplir les divers documents.</p>
+                                <a href="#" class="faq__button">Je filtre mes infos</a>
+                            </div>
+                        </div>
+                    </div>  
+                </div> 
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
     </div>
 </section>
 
