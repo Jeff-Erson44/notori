1367db98aca5f9f644a442575a852d8b42c4eddc
AntoineSalleras <antoinesalleras@hotmail.com>
2021-12-08 02:06:15 +0100

    new

--- public/includes/apropos.php
+++ public/includes/apropos.php
@@ -1,12 +1,12 @@
 <section id="apropos">
-    <h2 class="header2">Qu'est ce que Imminence</h2>
     <div class="apropos__container">
         <div class="apropos__container--video">
 
         </div>
         <div class="apropos__container--text">
-            <p>“Imminence” est un dispositif créé spécialement pour aider les futurs entrepreneurs innovants, mais aussi les entrepreneurs en général.</p>
-            <p>Si vous souhaitez vous lancer, mais que vous ne savez pas quelles sont les informations qui vous concernent, alors vous venez de trouver ce qu’il vous faut ! </p>
+            <h2 class="header2 header2__apropos">Qu'est ce que Imminence</h2>
+            <p class="paragraphe">“imminence” est un dispositif créé spécialement pour aider les futurs entrepreneurs innovants, mais aussi les entrepreneurs en général.</p>
+            <p class="paragraphe">Tout ce qui VOUS concernent et qui VOUS correspondent, regroupé au même endroit !</p>
         </div>
     </div>
 </section>
