7f8fdee5699d378240b752bce7c279afc90055ad
Elisabeth Ngo <elisab.ngo@gmail.com>
2021-12-10 00:21:26 +0100

    erreur 404

--- 404.php
+++ 404.php
@@ -1,7 +1,3 @@
-<?php
-require_once('public/includes/header.php');
-require_once('public/includes/footer.php');
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,8 +5,51 @@ require_once('public/includes/footer.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Erreur 404</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">   
+
+    <!-- CSS Slider -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
+    <!-- Mailchimp -->
+    <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+
+    <!--Font awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.0/css/all.min.css" crossorigin="anonymous"/>
+
+    <!--Font Poppins -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JH053D7RZL"></script>
+
+    <!-- Hotjar Tracking Code for My site -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2720314,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
 </head>
 <body>
-    <h1>Error 404</h1>
+    <div class="pointer"></div>
+    <div class="pointer2"></div>
+    <div class="error__container">
+        <h1 class="error__title">Erreur 404</h1>
+        <a class="error__btn button__primary" href="index.php">Retour sur la Landing Page imminence</a>
+    </div>
 </body>
+<script src="assets/js/script.js"></script>
 </html>
