72d0a9b77f5b708d51ce4daf52d9bbeecee6c3a2
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-09 22:22:43 +0100

    space responsive v1

--- public/includes/apropos.php
+++ public/includes/apropos.php
@@ -4,7 +4,7 @@
 
         </div>
         <div class="apropos__container--text">
-            <h2 class="header2 header2__apropos">Qu'est ce que <span>imminence</span> ?</h2>
+            <h2 class="header2 header2__apropos">Qu'est-ce que <span>imminence</span> ?</h2>
             <p class="paragraphe">“imminence” est un dispositif créé spécialement pour aider les futurs entrepreneurs innovants, mais aussi les entrepreneurs en général.</p>
             <p class="paragraphe apropos__paragraphe--second">Tout ce qui VOUS concernent et qui VOUS correspondent, regroupé au même endroit !</p>
             <button class="button__primary" type="button">Je commence mes démarches</button>
