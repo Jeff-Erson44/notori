c389fe7f4d98de28fb86c0f149675d196c08abb9
Elisabeth Ngo <elisab.ngo@gmail.com>
2021-12-09 21:38:09 +0100

    mailchimp newsletter style  responsive

--- public/includes/newsletter.php
+++ public/includes/newsletter.php
@@ -5,10 +5,10 @@
         <div id="mc_embed_signup_scroll">
         <label for="mce-EMAIL" class="mailchimp__title">Newsletter</label>
         <p>Tenez-nous au courant des dernières nouvelles :</p>
-        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Je saisie mon adresse email" required>
+        <input type="email" value="" name="EMAIL" class="newsletter__email" id="mce-EMAIL" placeholder="Je saisie mon adresse email" required>
         <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
         <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_cef9595e160134fb7481bf74e_c96b1b85f9" tabindex="-1" value=""></div>
-        <div class="clear"><input type="submit" value="Je m'inscris" name="s'inscrire" id="mc-embedded-subscribe" class="button"></div>
+        <div class="clear"><input type="submit" value="Je m'inscris" name="s'inscrire" id="mc-embedded-subscribe" class="button__primary newsletter__button"></div>
         </div>
     </form>
     </div>
