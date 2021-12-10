63fffd2e0843e01762effe0808025370768e2403
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2021-12-10 09:36:36 +0100

    adresse site

--- public/includes/head.php
+++ public/includes/head.php
@@ -14,7 +14,7 @@
 
 <meta property="og:title" content="Imminence" />
 <meta property="og:type" content="website" />
-<meta property="og:url" content="http://imminence-entrepreneur.fr" />
+<meta property="og:url" content="https://imminence-entrepreneur.com" />
 <meta property="og:image" content=/>
 <meta property="og:description" content="“imminence” est un dispositif créé spécialement pour aider les futurs entrepreneurs innovants, mais aussi les entrepreneurs en général.Si vous souhaitez vous lancer, mais que vous ne savez pas quelles sont les informations qui vous concernent, alors vous venez de trouver ce qu’il vous faut !" />
 <meta name="theme-color" content="#FF0000">
